58672c8fc14daad42411d6ad4a2aeb3040df98ca
Mahmoud Muhammad Sani <sanimahmoud10@gmail.com>
2025-01-25 17:57:28 +0100

    I Makes the footer year to displayinig correctly

--- src/App.tsx
+++ src/App.tsx
@@ -166,7 +166,7 @@ function App() {
 
         <footer className="bg-gray-900 text-white py-8">
           <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 text-center">
-            <p>© 2024 DevMahmoud. All rights reserved.</p>
+            <p>© { new Date().getFullYear() } DevMahmoud. All rights reserved.</p>
           </div>
         </footer>
 
